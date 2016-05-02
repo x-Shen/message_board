948395fdb1817f25304179a8210878b3b8c618d7
x-Shen <shenx3@uci.edu>
2016-05-02 16:25:56 -0700

    added signup page but not working

--- models/postMessageModel.js
+++ models/postMessageModel.js
@@ -1,3 +1,15 @@
 /**
  * Created by xinshen on 5/2/16.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostMessage = mongoose.model('postmessage',
+    {
+        _post: {type: Schema.Types.ObjectId, ref: 'post'},
+        message: String,
+        createdAt: { type: Date, default: Date.now },
+        user: Object
+    });
+
+module.exports = PostMessage;
