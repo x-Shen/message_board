00a09b514032bc2302bec6b00c6b9e2820ff7966
x-Shen <shenx3@uci.edu>
2016-05-02 16:40:08 -0700

    fixed broken link

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,10 @@
 /**
  * Created by xinshen on 5/2/16.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var user = mongoose.model('user',{
+    email:String,
+    password:String,
+})
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../models/userModel');
+var postModel = require('../models/postModel');
+var messageModel = require('../models/postMessageModel');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Message Board' }
+  );
 });
 
-router.get('/:user',function(req, res, next){
-  
+router.post('/signUp',function(req, res, next){
+  res.render('signup',{title:'Sign Up For Message Board',sub_title:'Please Fill the form to Sign Up'});
+  var newUser = new user(req.body);
+  newUser.save(function(err){
+    if(err) throw err;
+    res.redirect('/');
+  })
 });
+
+router.get('/logIn',function(req,res,next){
+  res.render('signup',{title:'Log In to Message Board',sub_title:'Please Fill the form to Log In'});
+  res.redirect('/');
+});
+
+
 
 module.exports = router;
