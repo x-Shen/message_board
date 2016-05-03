9f0030a6d684adce038b7b9ffe25a36331240eb1
x-Shen <shenx3@uci.edu>
2016-05-02 17:14:35 -0700

    start login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var userModel = require('../models/userModel');
+//var userModel = require('../models/userModel');
 var postModel = require('../models/postModel');
 var messageModel = require('../models/postMessageModel');
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var user = mongoose.model('user',{
+    email:String,
+    password:String
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,18 +18,25 @@ router.get('/', function(req, res, next) {
   );
 });
 
-router.post('/signUp',function(req, res, next){
+router.get('/signUp',function(req, res, next){
   res.render('signup',{title:'Sign Up For Message Board',sub_title:'Please Fill the form to Sign Up'});
-  var newUser = new user(req.body);
-  newUser.save(function(err){
-    if(err) throw err;
+
+});
+
+router.post('/saveUser', function(req,res,next){
+    var newUser = new user(req.body);
+    newUser.save(function(err){
+        if(err) throw err;
+    })
     res.redirect('/');
-  })
 });
 
 router.get('/logIn',function(req,res,next){
   res.render('signup',{title:'Log In to Message Board',sub_title:'Please Fill the form to Log In'});
 });
+
+
+
 
 
 module.exports = router;
